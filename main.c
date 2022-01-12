c7ebd9556e4200f1528be3faa9c97828651f8a3a
anish1231 <66822407+anish1231@users.noreply.github.com>
2022-01-12 14:24:59 +0530

    Done

---
+++ main.c
@@ -0,0 +1,19 @@
+//Calculating factorial of a large_number
+//
+//@Anish
+//12,January,2022
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "factorial.h"
+
+int main() {
+    printf("Enter a number :");
+    int n;
+    scanf("%d",&n);
+    fact(n);
+    return 0;
+
+}
